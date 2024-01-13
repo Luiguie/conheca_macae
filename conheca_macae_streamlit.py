038bc7c5b46d43c8119f6159457487acc2d0b97c
LGDS <gui.lgds2012@outlook.com>
2024-01-13 16:01:16 -0300

    Add Age Pyramid plot and text. Change .markdown to .write where needed

--- conheca_macae_streamlit.py
+++ conheca_macae_streamlit.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from IBGE_figs import *
-
+    
 def main():
     st.markdown("<h1 style='text-align: center;'>Conheça Macaé - Informações Sociais</h1><hr>", unsafe_allow_html=True)
     
-    st.markdown("""Bem-vindo à encantadora cidade de Macaé, localizada no interior do Rio de Janeiro. 
+    st.write("""Bem-vindo à encantadora cidade de Macaé, localizada no interior do Rio de Janeiro. 
         Conhecida por sua rica história e deslumbrante paisagem litorânea, Macaé destaca-se como 
         um importante polo econômico na região. Suas praias deslumbrantes, como a Praia dos 
         Cavaleiros, atraem visitantes em busca de tranquilidade e lazer. Além disso, a cidade é 
@@ -14,23 +14,37 @@ def main():
         da Princesinha do Atlântico."""
     )
     
-    col1, col2 = st.columns(2)
-    col1.plotly_chart(macae_cor_fig, use_container_width=True)
-    col2.plotly_chart(brasil_cor_fig, use_container_width=True)
+    col_cor_graph1, col_cor_graph2 = st.columns(2)
+    col_cor_graph1.plotly_chart(macae_cor_fig, use_container_width=True)
+    col_cor_graph2.plotly_chart(brasil_cor_fig, use_container_width=True)
     
-    st.markdown("""Macaé possui uma população residente ligeiramente menor de amarelos e indigenas se comparados
+    st.write("""Macaé possui uma população residente ligeiramente menor de amarelos e indigenas se comparados
                 com o panorama brasileiro. A distribuição de negros e brancos também varia em mais de cinco pontos percentuais,
                 com a população negra sendo 7.2% mais populosa, e a população branca sendo 6.1% menos populosa
                 na Capital Nacional da Energia.
                 """)
+    st.write("""A população vem estado em constante crescimento, quase dobrando em quantidade nos ultimos vinte anos,
+                seu crescimento está diretamente ligado ao mercado petrolifero que atrai cada vez mais trabalhadores das 
+                mais diversas regiões do Brasil em busca de oportunidades que a industria multibilionaria proporciona.
+                """)
     
-    
-    
-    
-    #st.dataframe(macae_population_df)
-    #st.dataframe(df_dist_idade_sexo)
-    #st.dataframe(macae_cor_fig)
     st.plotly_chart(macae_pop_fig)
+    
+    
+    col_txt_etaria, col_graph_etaria = st.columns(2)
+    with col_txt_etaria:
+        st.markdown("<br><br>",unsafe_allow_html=True)
+        st.write("""A distribuição entre a população macaense que se identifica como homens ou mulheres é uniforme para todas
+                                as idades, com a população feminina ultrapassando por pouco a masculina na maioria dos casos.
+                                """)
+        st.write("""Existe uma concentração maior de residentes entre os 30 e 45 anos, mostrando o envelhecimento gradual
+                                da Capital da Energia. Entretanto, seu Indice de Envelhecimento ainda se encontra em 40.58, ou seja, para 
+                                cada 100 pessoas de 15 anos ou menos, há cerca de 40 de 60 anos ou mais.
+                                """)
+        
+    with col_graph_etaria:
+        st.plotly_chart(piramide_etaria_fig)
+    
     st.plotly_chart(sun_pop_fig)
     
     
